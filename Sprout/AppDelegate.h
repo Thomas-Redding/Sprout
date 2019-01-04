59ccd0fd7f0d183ac29e402d129c06e0c0370c0e
Thomas Redding <thomasredding@Thomass-MacBook-Pro-2.local>
2019-01-04 00:02:59 -0800

    Iniital commit.

---
+++ Sprout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sprout
+//
+//  Created by Thomas Redding on 1/4/19.
+//  Copyright © 2019 Thomas Redding. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Sprout/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Sprout
+//
+//  Created by Thomas Redding on 1/4/19.
+//  Copyright © 2019 Thomas Redding. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
