71a1558ff44076042d1a6d5f4507c1acf14d9743
Thomas Redding <thomasredding@Thomass-MBP-3.lan1>
2019-06-11 20:04:43 -0500

    Added ability to sleep, restart, and shutdown.

--- Plugins/Launcher_System/Launcher_System.py
+++ Plugins/Launcher_System/Launcher_System.py
@@ -8,6 +8,12 @@ class Launcher_System:
             callback([('Launcher_System:quitSprout', 1000, 'quit Sprout')])
         if 'restart sprout'[0:len(userInput)] == userInput.lower():
             callback([('Launcher_System:restartSprout', 1000, 'restart Sprout')])
+        if 'sleep'[0:len(userInput)] == userInput.lower():
+            callback([('Launcher_System:sleepScreen', 1000, 'sleep')])
+        if 'shutdown'[0:len(userInput)] == userInput.lower():
+            callback([('Launcher_System:shutdown', 1000, 'shutdown')])
+        if 'restart'[0:len(userInput)] == userInput.lower():
+            callback([('Launcher_System:restart', 1000, 'restart')])
 
     def action(self, key, cmd, opt, ctrl, shift):
         if key == 'Launcher_System:quitSprout':
@@ -15,5 +21,13 @@ class Launcher_System:
             return True
         elif key == 'Launcher_System:restartSprout':
             self.spr.restartSprout()
+        elif key == 'Launcher_System:sleepScreen':
+            self.spr.sleepScreen()
+            return True
+        elif key == 'Launcher_System:shutdown':
+            self.spr.shutDown()
+            return True
+        elif key == 'Launcher_System:restart':
+            self.spr.restart()
             return True
         return False
